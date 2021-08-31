1e1a12643896e2e4adcc4a6451887ea1ddfaf620
Santiago Cuello <sjcuello@gmail.com>
2021-08-31 20:51:38 -0300

    Some changes

---
+++ carrousel/App.styled.js
@@ -0,0 +1,14 @@
+// import {Text} from 'react-native';
+// import styled from 'styled-components'
+
+// export const Tittle = styled(Text)({
+//     fontSize: '16px',
+//     textAlign: 'center',
+//   });
+
+import styled from 'styled-components/native'
+
+export const Tittle = styled.Text`
+  color: palevioletred;
+  textAlign: center
+`
